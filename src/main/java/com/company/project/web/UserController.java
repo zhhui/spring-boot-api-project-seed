dfa59f71c692efbed899a19a04545d9cb703fabc
zhonghui <53139222@qq.com>
2018-12-17 14:53:50 +0800

    demo spring seed project

---
+++ src/main/java/com/company/project/web/UserController.java
@@ -0,0 +1,65 @@
+package com.company.project.web;
+
+import com.company.project.core.Result;
+import com.company.project.core.ResultGenerator;
+import com.company.project.model.User;
+import com.company.project.service.UserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Condition;
+
+/**
+* Created by CodeGenerator on 2017/10/13.
+*/
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @PostMapping
+    public Result add(@RequestBody User user) {
+        userService.save(user);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @DeleteMapping("/{id}")
+    public Result delete(@PathVariable Integer id) {
+        userService.deleteById(id);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @PutMapping
+    public Result update(@RequestBody User user) {
+        userService.update(user);
+        return ResultGenerator.genSuccessResult();
+    }
+
+    @GetMapping("/{id}")
+    public Result detail(@PathVariable Integer id) {
+        User user = userService.findById(id);
+        return ResultGenerator.genSuccessResult(user);
+    }
+
+    @GetMapping
+    public Result list(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "0") Integer size,
+        User user) {
+        PageHelper.startPage(page, size);
+        Condition condition = new Condition(user.getClass());
+        condition.createCriteria().andLike("nickName",user.getNickName());
+        List<User> list = userService.findByCondition(condition);
+        PageInfo pageInfo = new PageInfo(list);
+        return ResultGenerator.genSuccessResult(pageInfo);
+    }
+}
